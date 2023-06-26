6664e840414799ae7f48f428875c75942bff80de
Evan / Fero <hi@evan.graphics>
2023-06-26 09:27:31 -0700

    fix: light mode dropdown menus

--- components/Primitives/Dropdown.tsx
+++ components/Primitives/Dropdown.tsx
@@ -98,7 +98,7 @@ export function RadioDropdown({
 
       <DropdownMenu.Portal>
         {/* hot take, i actually think that forcing the dropdowns to be in dark mode has better contrast */}
-        <div className={`contents ${montserrat}`}>
+        <div className={`dark contents ${montserrat}`}>
           {/* bg-base-3-light dark:bg-base-3-dark w-[250px] text-black dark:text-white rounded-xl border-2 border-borders-base2-light dark:border-borders-base2-dark  */}
           <DropdownMenu.Content
             avoidCollisions
