b96fafe882cd2c7c3ff64690d3b3f190b4f494f3
Beebles <102569435+beebls@users.noreply.github.com>
2023-06-28 14:01:40 -0600

    Fix: Adding a theme to a preset no longer reloads backend

--- components/Presets/AddThemeToPresetButton.tsx
+++ components/Presets/AddThemeToPresetButton.tsx
@@ -64,7 +64,7 @@ export function AddThemeToPresetButton() {
                 ...selectedPreset.dependencies,
                 themeToAdd,
               ]).then(() => {
-                refreshThemes(true);
+                refreshThemes();
               });
             }
           }}
