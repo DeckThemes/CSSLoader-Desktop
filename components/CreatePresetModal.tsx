0d1350de1f693fa45da40ef5f75f5aade69ecaa2
Beebles <102569435+beebls@users.noreply.github.com>
2023-06-27 23:21:57 -0600

    fix: Fix nested buttons in CreatePresetModal

--- components/CreatePresetModal.tsx
+++ components/CreatePresetModal.tsx
@@ -51,15 +51,14 @@ export function CreatePresetModal() {
         } you currently have enabled. Enabling/disabling it will toggle them all at once.`}
         triggerDisabled={enabledThemes === 0}
         Trigger={
-          <button
-            disabled={enabledThemes === 0}
+          <div
             className={`flex h-8 w-fit items-center justify-center gap-2 whitespace-nowrap rounded-full border-2 border-[#2e2e2e] px-4 py-2 text-sm font-bold ring-brandBlue transition duration-100 focus:ring-2 ${
               enabledThemes > 0 ? "bg-[#2563eb]" : "pointer-events-none opacity-50"
             }`}
           >
             <BiPlus />
             <span>New Preset</span>
-          </button>
+          </div>
         }
       />
     </>
