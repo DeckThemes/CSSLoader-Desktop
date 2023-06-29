64a7834c6d414f57826e7f534c1adff750f3ffba
Evan / Fero <hi@evan.graphics>
2023-06-28 21:39:54 -0700

    fix: <button> navtabs to prevent shift-clicking route links that open a new window
    conflicts with prefetching link

--- components/Nav/NavTab.tsx
+++ components/Nav/NavTab.tsx
@@ -4,19 +4,26 @@ import { useRouter } from "next/router";
 export function NavTab({ href, name, icon }: { href: string; name: string; icon: any }) {
   const router = useRouter();
 
+  const handleKeyDown = (e: KeyboardEvent, href: string) => {
+	if (e.key === "Enter") {
+		router.push(href)
+	}
+  }
+
   return (
     <>
-      <Link
-        href={href}
+      <button
+	  	role="link"
+	  	onClick={() => router.push(href)}
+		onKeyDown={(e) => handleKeyDown(e, href)}
         style={{
           background: router.pathname === href ? "rgb(37, 99, 235)" : "#1e2024",
-          //   color: router.pathname === href ? "rgb(48,48,48)" : "",
         }}
         className="flex h-fit items-center justify-center gap-2 rounded-full bg-elevation-2-dark px-4 py-2 transition-all duration-150 hover:scale-95 hover:active:scale-90"
       >
         <div className="flex">{icon}</div>
         <span className="text-sm font-bold">{name}</span>
-      </Link>
+      </button>
     </>
   );
 }
