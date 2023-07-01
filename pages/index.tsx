61f7a7a65209e3e1c8a52b8717ea9998265e79f4
Beebles <102569435+beebls@users.noreply.github.com>
2023-07-01 15:36:33 -0600

    feat: Store sort and display values in store

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,13 +13,25 @@ import { LabelledInput, RadioDropdown, Tooltip, TwoItemToggle } from "@component
 import { TbColumns1, TbColumns2 } from "react-icons/tb";
 import { Flags } from "ThemeTypes";
 import Link from "next/link";
+import { storeRead, storeWrite } from "backend";
 
 export default function MainPage() {
   const vw = useVW();
-  const { refreshThemes, themes, selectedPreset } = useContext(themeContext);
+  const { refreshThemes, themes } = useContext(themeContext);
   const [search, setSearch] = useState<string>("");
   const [numCols, setNumCols] = useState<number>(1);
   const [sortValue, setSort] = useState<any>("nameAZ");
+
+  useEffect(() => {
+    storeRead("desktopDisplay").then((res) => {
+      if (!res.success) return;
+      if (res.result === "1" || res.result === "2") setNumCols(Number(res.result));
+    });
+    storeRead("desktopSort").then((res) => {
+      if (!res.success || res?.result?.length === 0) return;
+      setSort(res.result);
+    });
+  }, []);
 
   const [sortedThemes] = useMemo(() => {
     const filteredAll = themes.filter(
@@ -60,6 +72,7 @@ export default function MainPage() {
   useEffect(() => {
     if (vw < 650 && numCols === 2) {
       setNumCols(1);
+      storeWrite("desktopDisplay", "1");
     }
   }, [vw]);
 
@@ -82,7 +95,10 @@ export default function MainPage() {
                 ariaLabel="Sort By Dropdown"
                 headingText="Sort By"
                 value={sortValue}
-                onValueChange={setSort}
+                onValueChange={(value) => {
+                  setSort(value);
+                  storeWrite("desktopSort", value);
+                }}
                 options={[
                   { value: "nameAZ", displayText: "Theme Name (A to Z)" },
                   { value: "nameZA", displayText: "Theme Name (Z to A)" },
@@ -103,7 +119,10 @@ export default function MainPage() {
                   { value: 1, displayText: <TbColumns1 size={18} /> },
                   { value: 2, displayText: <TbColumns2 size={18} /> },
                 ]}
-                onValueChange={setNumCols}
+                onValueChange={(value) => {
+                  setNumCols(value);
+                  storeWrite("desktopDisplay", value + "");
+                }}
               />
             </div>
             <div className="flex w-full max-w-5xl items-end justify-center gap-4"></div>
