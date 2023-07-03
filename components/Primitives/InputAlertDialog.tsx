6924092d5a465ccff8bcc2285dd56e73552007b3
Evan / Fero <hi@evan.graphics>
2023-07-02 21:42:23 -0700

    chore: wrap inputalertdialog with <form>

--- components/Primitives/InputAlertDialog.tsx
+++ components/Primitives/InputAlertDialog.tsx
@@ -1,5 +1,5 @@
 import { AlertDialog } from "./AlertDialog";
-import { ReactNode, useState } from "react";
+import { FormEvent, ReactNode, useState } from "react";
 import { LabelledInput } from "./LabelledField";
 
 export function InputAlertDialog(props: {
@@ -27,27 +27,33 @@ export function InputAlertDialog(props: {
 }) {
   const [inputValue, setInputValue] = useState("");
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (props.validateInput(inputValue)) {
+      props.onAction(inputValue);
+    }
+  };
+
   return (
     <AlertDialog
       {...props}
       onAction={() => {
-        props.onAction(inputValue);
+        if (props.validateInput(inputValue)) {
+          props.onAction(inputValue);
+        }
       }}
       actionDisabled={!props.validateInput(inputValue)}
       Content={
-        <div className="flex w-full items-end gap-2 px-4 pb-4">
-          <LabelledInput
-            onKeyDown={(e) => {
-              if (e.key === "Enter" && props.validateInput(inputValue)) {
-                props.onAction(inputValue);
-              }
-            }}
-            inputClass="bg-base-5.5-light dark:bg-base-5.5-dark"
-            label={props.labelText}
-            value={inputValue}
-            onValueChange={setInputValue}
-          />
-        </div>
+        <form className="contents" onSubmit={handleSubmit}>
+          <div className="flex w-full items-end gap-2 px-4 pb-4">
+            <LabelledInput
+              inputClass="bg-base-5.5-light dark:bg-base-5.5-dark"
+              label={props.labelText}
+              value={inputValue}
+              onValueChange={setInputValue}
+            />
+          </div>
+        </form>
       }
     />
   );
